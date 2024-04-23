4496d5b7feaab87bd0e7db7e4ca9af74a0e7348b
Matthew Taylor <matthewtayl@fmgl.com.au>
2024-04-23 08:16:19 +0800

    respond to initial request with a 200 response

--- app/server.go
+++ app/server.go
@@ -8,15 +8,21 @@ import (
 
 func main() {
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	connection, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
 		os.Exit(1)
 	}
 }
