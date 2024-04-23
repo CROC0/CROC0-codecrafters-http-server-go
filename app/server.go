54b1036f1b7b238026925afaac6e6d31e445a30d
Matthew Taylor <matthewtayl@fmgl.com.au>
2024-04-23 10:22:01 +0800

    check path and return based on value

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 	"os"
@@ -19,10 +20,29 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-
-	_, err = connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	buffer := make([]byte, 1028)
+	_, err = connection.Read(buffer)
 	if err != nil {
 		fmt.Println("Error writing to connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	header := bytes.Split(buffer, []byte("\r\n"))
+	startLine := bytes.Split(header[0], []byte(" "))
+	// _method := startLine[0]
+	path := startLine[1]
+	// _httpVersion := startLine[2]
+
+	if string(path) == "/" {
+		_, err = connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		_, err = connection.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+
+	}
+
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
+		os.Exit(1)
+	}
+
 }
